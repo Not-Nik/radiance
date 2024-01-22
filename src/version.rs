f46b5e09102f3e227cfedf87ba907b435a35de54
Not-Nik <nik.wipper@gmx.de>
2024-01-22 19:52:04 +0100

    Add license and copyright header

--- src/version.rs
+++ src/version.rs
@@ -1,3 +1,9 @@
+// radiance (c) Nikolas Wipper 2024
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 use crate::version::Platform::{Linux, OsX, Win};
 use enum_map::Enum;
 use std::cmp::Ordering;
