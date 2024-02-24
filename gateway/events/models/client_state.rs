2111769b4a33637d3244fa4ad5b0a6456280f4a0
Not-Nik <nik.wipper@gmx.de>
2024-02-24 19:25:15 +0100

    [gateway] Make some ClientState members optional

--- gateway/events/models/client_state.rs
+++ gateway/events/models/client_state.rs
@@ -10,9 +10,9 @@ use std::collections::HashMap;
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct ClientState {
     pub guild_versions: HashMap<(), ()>,
-    pub highest_last_message_id: String, // this is an id, or zero, so we can't just put Id as the type
-    pub read_state_version: u32,
-    pub user_guild_settings_version: i32,
-    pub private_channels_version: String,
-    pub api_code_version: u32,
+    pub highest_last_message_id: Option<String>, // this is an id, or zero, so we can't just put Id as the type
+    pub read_state_version: Option<u32>,
+    pub user_guild_settings_version: Option<i32>,
+    pub private_channels_version: Option<String>,
+    pub api_code_version: Option<u32>,
 }
